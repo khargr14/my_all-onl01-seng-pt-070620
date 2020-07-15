d9b832c47d1a23fbbc4e2d1c06ba1378e915d5c1
Keesha Hargrove <kharg14@gmail.com>
2020-07-15 01:07:18 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,15 @@
-require 'pry'
-
 def my_all?(collection)
+  i = 0
+    while i < collection.length
+    
+    yield (collection[i]) 
+    i = i + 1
+  end
+  
+if block_return_values =
+  
+
+
+
 
 end
